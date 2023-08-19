899ae15afde052d71ff6271f47be710c59956a40
LoKyOnTheCode <97956863+LoKyOnTheCode@users.noreply.github.com>
2023-08-19 17:20:48 +0200

    Update ilnux.sh

--- scripts/ilnux.sh
+++ scripts/ilnux.sh
@@ -17,14 +17,5 @@ echo -e "\n${bold}Kernel version : ${end}" && uname -v
 echo -e "\n${bold}Hardware architecture : ${end}" && uname -i
 echo -e "\n${bold}OS : ${end}" && uname -o
 
-echo -e "\n\n===========+===========\n${bold}Checking directories...${end}\n===========+==========="
-echo -e "\n${byellow}/var/www/ ${end}\n=======+=======" && ls -laR --color=auto /var/www/
-echo -e "\n${byellow}/home/${end}\n=======+=======" && ls -la --color=auto /root/ 2>/dev/null || echo "You are not root :/" && ls -laR --color=auto /home/${USER}
-echo -e "\n${byellow}/opt/${end}\n=======+=======" && ls -laR --color=auto /opt
-echo -e "\n${byellow}/tmp/${end}\n=======+=======" && ls -laR --color=auto /tmp
-
-echo -e "\n\n===========+===========\n${bold}Checking sudo right${end}\n===========+===========\n"
-sudo -l || echo "Can't check for ${USER}"
-
 echo -e "\n\n===========+===========\n${bold}Checking binaries w/ root perm${end}\n===========+===========\n"
 find / -user root -perm -4000 -exec ls -ldb {} \; 2>/dev/null
